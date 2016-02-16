d6428726b8e20b30db87940feaf03c420ef7b21f
jnaldz000 <justin_naldzin@cable.comcast.com>
2016-02-16 14:39:40 -0500

    quotes

--- js/config.js
+++ js/config.js
@@ -22,19 +22,46 @@ var config = {
         morning: [
             'Good morning, handsome!',
             'Enjoy your day!',
-            'How was your sleep?'
+            'How was your sleep?',
+            "A true hero isn't measured by the size of his strength, but by the strength of his heart.\nHercules",
+            "The past can hurt. But the way I see it, you can either run from it, or learn from it.\nThe Lion King",
+            "If you keep on believing the dreams that you wish will come true.\nCinderella",
+            "Some people care too much ... I think it's called love.\nWinnie the Pooh",
+            "Just because I cannot see it, doesn't mean I can't believe it.\nA Nightmare Before Christmas",
+            "The way to get started is to quit talking and begin doing.\nWalt Disney",
+            "If you can't say somethin' nice, don't say nothin' at all!\nBambi",
+            "I'd rather die tomorrow than live a hundred years without you.\nPocahontas",
+            "I never look back, darling. It distracts me from the now.\nThe Incredibles",
+            "Don't just fly, soar.\nDumbo"
         ],
         afternoon: [
             'Hello, beauty!',
-            'You look sexy!',
             'Looking good!',
             'Hey buddy!',
-            'Sup'
+            'Sup',
+            "All our dreams can come true if we have the courage to pursue them.\nWalt Disney",
+            "Happiness is a state of mind. It's just according to the way you look at things.\nWalt Disney",
+            "I give myself very good advice, but I very seldom follow it.\nAlice In Wonderland",
+            "When you got your pals, you got all you need.\nOliver & Company",
+            "Always let your conscience be your guide.\nPinocchio",
+            "Giving up is for rookies.\nHercules",
+            "Life's not a spectator sport. If watchin' is all you're gonna do, then you're gonna watch your life go by without ya.\nThe Hunchback of Notre Dame",
+            "A lie keeps growing and growing until it's as plain as the nose on your face.\nPinocchio",
+            "A single grain of rice can tip the scale. One man may be the difference between victory and defeat.\nMulan",
+            "In every job that must be done there is an element of fun.\nMary Poppins"
         ],
         evening: [
-            'Wow, you look hot!',
-            'You look nice!',
-            'Hi, sexy!'
+            "Sometimes the smallest things take up the most room in your heart.\nWinnie The Pooh",
+            "The things that make me different are the things that make me.\nWinnie The Pooh",
+            "Today is a good day to try.\nHunchback of Notre Dame",
+            "Like so many things, it is not what is outside, but what is inside that counts.\nAladdin",
+            "The flower that blooms in adversity is the most rare and beautiful of all.\nMulan",
+            "Believe you can, then you will.\nMulan",
+            "If you hear music, dance.\nSleeping Beauty",
+            "You're never too old to wish upon a star.\nThe Princess and the Frog",
+            "The path to your destiny lies within the magic of your heart.\nTangled",
+            "You are braver than you believe, stronger than you seem, and smarter than you think.\nWinnie The Pooh",
+            "Even miracles take a little time.\nCinderella"
         ]
     },
     calendar: {
